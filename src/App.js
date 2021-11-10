5b8eefd9992b49f1931bfc5b9780bc14dffb0317
Miguel Montiel <migueel.angelom@gmail.com>
2021-11-10 14:47:48 -0600

    Adicion de librerias y organizacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import CharacterList from './components/CharacterList'
+import CharacterList from './components/CharactersList'
+import Pagination from './components/Pagination';
 
 const App = () => {
 
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
+  const [paginationInfo, setPaginationInfo] = useState([])
 
   // DECLARING THE URL TO OBTAIN DATA
   let url_page = 'https://rickandmortyapi.com/api/character';
@@ -12,20 +14,31 @@ const App = () => {
   const fetch_characters = (url) => {
     fetch(url)
       .then(response => response.json())
-      .then(data => setCharacters(data.results))
+      .then(data => {
+        setCharacters(data.results);
+        setPaginationInfo(data.info);
+      })
       .catch(error => console.log(error))
   };
-  
+
   useEffect(() => {
     fetch_characters(url_page);
-  }, []);
+  }, [url_page]);
 
   return (
     <>
-      <h1 className='h1 text-center'>
-        Rick n Morty WEB APP
-      </h1>
-      <CharacterList characters={characters}/>
+      <div className='container-fluid py-4'>
+        <h1 className='h1 text-center text-primary my-2'>
+          Rick and Morty WEB APP
+        </h1>
+
+        <div className='bg-white rounded' style={{ margin: '10px 0px', padding: 10}}>          
+          <h2 className="h2 text-center mt-2">Characters List</h2>
+          <Pagination paginationInfo={paginationInfo} />
+          <CharacterList characters={characters} />          
+          <Pagination paginationInfo={paginationInfo} />
+        </div>
+      </div>
     </>
   );
 }
