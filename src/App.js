22e1e942968201734b577ef5ec713ebee8660588
Miguel Montiel <migueel.angelom@gmail.com>
2021-11-10 18:51:48 -0600

    Configuracion de logica del paginador

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import CharacterList from './components/CharactersList'
+import Header from './components/Header';
 import Pagination from './components/Pagination';
 
 const App = () => {
 
   const [characters, setCharacters] = useState([]);
   const [paginationInfo, setPaginationInfo] = useState([])
-
+  
   // DECLARING THE URL TO OBTAIN DATA
-  let url_page = 'https://rickandmortyapi.com/api/character';
+  const [url_page, setUrl_page] = useState('https://rickandmortyapi.com/api/character')
+  // let url_page = 'https://rickandmortyapi.com/api/character';
 
   // OBTAINING DATA FROM API
-  const fetch_characters = (url) => {
-    fetch(url)
+  const fetch_characters = async (url) => {
+    await fetch(url)
       .then(response => response.json())
       .then(data => {
         setCharacters(data.results);
@@ -23,21 +25,19 @@ const App = () => {
 
   useEffect(() => {
     fetch_characters(url_page);
-    console.log(paginationInfo);
+    // console.log(paginationInfo);
   }, [url_page]);
 
   return (
     <>
       <div className='container-fluid py-4'>
-        <h1 className='h1 text-center text-primary my-2'>
-          Rick and Morty WEB APP
-        </h1>
+        
+        <Header />
 
         <div className='bg-white rounded' style={{ margin: '10px 0px', padding: 10}}>          
           <h2 className="h2 text-center mt-2">Characters List</h2>
-          <Pagination paginationInfo={paginationInfo} />
-          <CharacterList characters={characters} />          
-          <Pagination paginationInfo={paginationInfo} />
+          <Pagination paginationInfo={paginationInfo} setUrl_page={setUrl_page} />
+          <CharacterList characters={characters} />
         </div>
       </div>
     </>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,31 +1,101 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+// import PaginationNumbers from './PaginationNumbers';
 
-const Pagination = () => {
+const enablePrev = (prev_exists) => {
+    if (prev_exists != null) {
+        return '';
+    } else {
+        return 'disabled';
+    }
+}
+
+const enableNext = (next_exists) => {
+    if (next_exists != null) {
+        return '';
+    } else {
+        return 'disabled';
+    }
+}
+
+// const getNumbering = (no_pages) => {
+//     let pageNumbers = [];
+//     for (let index = 0; index < no_pages; index++) {
+//         pageNumbers.push(index + 1)
+//     }
+//     return pageNumbers;
+// }
+
+const Pagination = ({ paginationInfo, setUrl_page }) => {
+
+    // let enable_prev = 'disabled';
+
+    // BUTTONS TO ENABLE PREV AND NEXT ITEMS
+    let enable_prev = enablePrev(paginationInfo.prev);
+    let enable_next = enableNext(paginationInfo.next);
+
+    // let pageNumbers = [];
+    // let no_pages = 1;
+
+    // for (let index = 0; index < paginationInfo.pages; index++) {
+    //     pageNumbers.push(index + 1)
+    // }
+
+    // let actual_page = '';
+    // pageNumbers = getNumbering(paginationInfo.pages);
+    
+    enable_prev = (paginationInfo.prev == null) ? 'disabled' : '';
+    enable_next = (paginationInfo.next == null) ? 'disabled' : '';
+
+    useEffect(() => {
+        // actual_page = paginationInfo.next;
+        // actual_page = actual_page.slice(-1);
+        console.log(paginationInfo.next);
+        // console.log(pageNumbers);
+
+    }, [paginationInfo])
+
     return (
         <div className='mt-3 d-flex' style={{}}>
-        <nav style={{margin: '0 auto'}} aria-label="Page navigation example">
-            <ul className="pagination pagination-circle pg-blue">
-                <li className="page-item disabled"><button className="page-link">First</button></li>
-                <li className="page-item disabled">
-                    <button className="page-link" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span className="sr-only">Previous</span>
-                    </button>
-                </li>
-                <li className="page-item active"><button className="page-link">1</button></li>
-                <li className="page-item"><button className="page-link">2</button></li>
-                <li className="page-item"><button className="page-link">3</button></li>
-                <li className="page-item"><button className="page-link">4</button></li>
-                <li className="page-item"><button className="page-link">5</button></li>
-                <li className="page-item">
-                    <button className="page-link" aria-label="Next">
-                        <span>&raquo;</span>
-                        <span className="sr-only">Next</span>
-                    </button>
-                </li>
-                <li className="page-item"><button className="page-link">Last</button></li>
-            </ul>
-        </nav>
+            <nav style={{ margin: '0 auto' }} aria-label="Page navigation example">
+                <ul className="pagination pagination-circle pg-blue">
+
+                    {/* FIRST AND PREVIOUS ITEMS */}
+                    {/* <li className={"page-item " + enable_prev}>
+                        <button className="page-link">First</button>
+                    </li> */}
+                    <li className={"page-item " + enable_prev}>
+                        <button
+                            className="page-link"
+                            onClick={() => setUrl_page(paginationInfo.prev)}
+                            aria-label="Previous">
+                            <span aria-hidden="true">&laquo; Previous</span>
+                            <span className="sr-only">Previous</span>
+                        </button>
+                    </li>
+
+
+                    {/* MOSTRAMOS EL NUMERO DE PAGINAS */}
+                    {
+                        // pageNumbers.map((value, index) => {
+                        //     return <PaginationNumbers no_page={index} actual_page={value} />
+                        // })
+                    }
+
+
+                    {/* NEXT AND LAST ITEMS */}
+                    <li className={"page-item " + enable_next}>
+                        <button className="page-link"
+                            onClick={() => setUrl_page(paginationInfo.next)}
+                            aria-label="Next">
+                            <span>Next &raquo;</span>
+                            <span className="sr-only">Next</span>
+                        </button>
+                    </li>
+                    {/* <li className={"page-item " + enable_next}>
+                        <button className="page-link">Last</button>
+                    </li> */}
+                </ul>
+            </nav>
         </div>
     )
 }
